03d07b6003a70c3758a5428c3516b694f1f7313c
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-09 15:57:07 +0200

    wrote the algorithm in script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//algorithm
+/*
+-ask the user for the kilometres to travel (first variable)
+-ask the user for the age of the passenger (second variable)
+-create a variable for the price per km
+-create a variable for under18 price
+-create a variable for over65 price
+-write a conditional statement with the full price and the 2 discount price:
+-- if age < 18 apply -20%
+   if age >= 18 && <= 65 apply full price
+   if age > 65 apply -40%
+
+-print proce with 2 decimal number    
+
+
+*/
